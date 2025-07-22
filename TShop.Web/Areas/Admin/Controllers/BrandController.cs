12e4db6579c97c2093dca058512f2a0361773ee9
Tofa <thofazzolhossen@gmail.com>
2025-07-22 19:54:19 +0600

    Add Brand View Model

---
+++ TShop.Web/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TShop.Web.ViewModels;
+
+namespace TShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(BrandVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to add the brand using a service or repository
+                // await _brandService.AddAsync(model);
+                Console.WriteLine($"Brand Created: {model.Name}");
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
