b8146ed167d98c3430bd5c2729b3a2290a8bb971
Tofa <thofazzolhossen@gmail.com>
2025-07-23 16:38:58 +0600

    Add BrandVM

---
+++ TShop.Web/ViewModels/BrandVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TShop.Web.ViewModels
+{
+    public class BrandVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "Name cannot be longer than 30 characters.")]
+        [Display(Name = "Brand Name")]
+        [RegularExpression(@"^[a-zA-Z0-9\s_-]+$", ErrorMessage = "Name can only contain letters, numbers, and spaces.")]
+        public string Name { get; set; }
+    }
+}
